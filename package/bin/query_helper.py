8fd6bcef274ef70954fb00e6b98a4d89ecf5b8bb
Erica Pescio <edro15@users.noreply.github.com>
2025-10-24 13:51:26 +0200

    style:fixes for linting

--- package/bin/query_helper.py
+++ package/bin/query_helper.py
@@ -3,13 +3,13 @@ import logging
 import requests
 from requests.auth import HTTPBasicAuth
 
-import import_declare_test
 from solnlib import conf_manager, log
 from solnlib.modular_input import checkpointer
 from splunklib import modularinput as smi
 
 
 ADDON_NAME = "solarwinds_ta"
+
 
 def logger_for_input(input_name: str) -> logging.Logger:
     return log.Logs().get_logger(f"{ADDON_NAME.lower()}_{input_name}")
@@ -27,10 +27,7 @@ def get_account_property(session_key: str, account_name: str, property_name: str
 
 def get_data_from_api(logger: logging.Logger, username, password, url):
     logger.info("Getting data from an external API")
-    response = requests.get(
-        url,
-        auth=HTTPBasicAuth(username, password)
-    )
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
 
     logger.debug(f"Response status code: {response.status_code}")
     logger.debug(f"Response: {response.json()}")
@@ -43,7 +40,6 @@ def validate_input(definition: smi.ValidationDefinition):
 
 
 def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
-
     for input_name, input_item in inputs.inputs.items():
         normalized_input_name = input_name.split("/")[-1]
         logger = logger_for_input(normalized_input_name)
@@ -63,10 +59,15 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
             )
             logger.setLevel(log_level)
             log.modular_input_start(logger, normalized_input_name)
-            server = get_account_property(session_key, input_item.get("account"), "server")
-            port = get_account_property(session_key, input_item.get("account"), "port")
-            username = get_account_property(session_key, input_item.get("account"), "username")
-            password = get_account_property(session_key, input_item.get("account"), "password")
+            server = get_account_property(
+                session_key, input_item.get("account"), "server"
+            )
+            username = get_account_property(
+                session_key, input_item.get("account"), "username"
+            )
+            password = get_account_property(
+                session_key, input_item.get("account"), "password"
+            )
             query = input_item.get("Query", "")
             checkpoint_field = input_item.get("checkpoint_field", "")
 
@@ -75,10 +76,10 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
             request_url = f"{server}/SolarWinds/InformationService/v3/Json/Query?query={query_formatted}"
             logger.debug(f"Query URL: {request_url}")
             response = get_data_from_api(logger, username, password, request_url)
-            sourcetype="json"
+            sourcetype = "json"
             event_counter = 0
             if response.status_code == 200:
-                data = response.json()['results']
+                data = response.json()["results"]
                 for event in data:
                     checkpoint = event[checkpoint_field]
                     if kvstore_checkpointer.get(checkpoint) is None:
@@ -86,13 +87,15 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
                             smi.Event(
                                 data=json.dumps(event, ensure_ascii=False, default=str),
                                 index=input_item.get("index"),
-                                sourcetype=sourcetype
+                                sourcetype=sourcetype,
                             )
                         )
                         event_counter += 1
-                        kvstore_checkpointer.update(checkpoint, 'saved')
+                        kvstore_checkpointer.update(checkpoint, "saved")
                     else:
-                        logger.debug(f"Checkpoint already exists for {checkpoint}, skipping event")
+                        logger.debug(
+                            f"Checkpoint already exists for {checkpoint}, skipping event"
+                        )
 
             log.events_ingested(
                 logger,
@@ -104,4 +107,9 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
             )
             log.modular_input_end(logger, normalized_input_name)
         except Exception as e:
-            log.log_exception(logger, e, "Ingestion error", msg_before=f"Exception raised while ingesting data for input: {normalized_input_name}")
+            log.log_exception(
+                logger,
+                e,
+                "Ingestion error",
+                msg_before=f"Exception raised while ingesting data for input: {normalized_input_name}",
+            )

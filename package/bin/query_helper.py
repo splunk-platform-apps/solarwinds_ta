4e6c47a5ec864dc9302317fdf424936dd16816aa
Erica Pescio <edro15@users.noreply.github.com>
2025-10-27 11:24:08 +0100

    style:as per linting request

--- package/bin/query_helper.py
+++ package/bin/query_helper.py
@@ -81,7 +81,9 @@ def stream_events(inputs: smi.InputDefinition, event_writer: smi.EventWriter):
                 data = response.json()["results"]
                 for event in data:
                     if checkpoint_field not in event:
-                        raise Exception(f"{checkpoint_field} not found in the event: {event}")
+                        raise Exception(
+                            f"{checkpoint_field} not found in the event: {event}"
+                        )
                     checkpoint = event[checkpoint_field]
                     if kvstore_checkpointer.get(checkpoint) is None:
                         event_writer.write_event(
